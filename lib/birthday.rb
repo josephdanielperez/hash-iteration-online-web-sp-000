f8e9de7e875f73a4fc5d9a65907fef99ce778945
Joseph Perez <hello@josephdanielperez.com>
2020-07-02 03:56:35 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,6 +6,6 @@ birthday_kids = {
 
 def happy_birthday(birthday_kids)
   birthday_kids.each do |name, age|
-    puts "Happy birthday #{name}!"
+    puts "Happy Birthday #{name}! You are now #{age} years old!"
   end
 end
